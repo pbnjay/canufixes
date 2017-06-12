1a22e2a00977ded34603c608843ad43ead2f4fd5
Jeremy Jay <jeremy@pbnjay.com>
2017-06-12 14:34:03 -0400

    initial import

---
+++ mergeclears/main.go
@@ -0,0 +1,83 @@
+// Command mergeclears merges multiple .clear files produced from CANU's
+// splitReads commands (run in parallel). CANU's code doesn't have a way to
+// merge these files, and crashes for unknown reasons during splitReads.
+//
+// We externally parallelized canu's splitReads step to avoid the crash, so
+// we needed this code to merge the results back together.
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"os"
+)
+
+func main() {
+	bestOnly := flag.Bool("b", true, "take best from all files (max begin, min end)")
+	outname := flag.String("o", "merged.clear", "output clear file")
+	flag.Parse()
+
+	var numReads uint32
+	var alldata []uint32
+	for _, fn := range flag.Args() {
+		f, err := os.Open(fn)
+		if err != nil {
+			panic(err)
+		}
+		var nreads uint32
+		err = binary.Read(f, binary.LittleEndian, &nreads)
+		if err != nil {
+			panic(err)
+		}
+		if numReads == 0 {
+			numReads = nreads
+		}
+		if numReads != nreads {
+			panic("numreads doesn't match in all files")
+		}
+		data := make([]uint32, nreads)
+		err = binary.Read(f, binary.LittleEndian, data)
+		if err != nil {
+			panic(err)
+		}
+		f.Close()
+
+		if alldata == nil {
+			alldata = data
+			continue
+		}
+		if *bestOnly {
+			for i := uint32(0); i < nreads; i++ {
+				if alldata[i] < data[i] { // begins
+					alldata[i] = data[i]
+				}
+				if data[i+nreads] != 0 && alldata[i+nreads] > data[i+nreads] { // ends
+					alldata[i+nreads] = data[i+nreads]
+				}
+			}
+		} else {
+			for i := uint32(0); i < nreads; i++ {
+				if alldata[i] == 0 { // begins
+					alldata[i] = data[i]
+				}
+				if alldata[i+nreads] == 0 { // ends
+					alldata[i+nreads] = data[i+nreads]
+				}
+			}
+		}
+	}
+
+	f, err := os.Create(*outname)
+	if err != nil {
+		panic(err)
+	}
+	err = binary.Write(f, binary.LittleEndian, numReads)
+	if err != nil {
+		panic(err)
+	}
+	err = binary.Write(f, binary.LittleEndian, alldata)
+	if err != nil {
+		panic(err)
+	}
+	f.Close()
+}
